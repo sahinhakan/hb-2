9f8e1649132e75c3bf489e79a69a20e13f3cf9df
Hakan Sahin <hakansahince@gmail.com>
2022-02-02 16:45:53 +0300

    ListItem component css file is added and detail div is added.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,27 @@
 import { Button, Row, Col } from "antd";
+import { SearchOutlined, ArrowUpOutlined, ArrowDownOutlined} from '@ant-design/icons';
+import './ListItem.css';
 
 const ListItem = () => {
     return (
         <>
-        <Row>
+        <Row className="row">
             <Col span={5}>
-                <div style={{ backgroundColor: "#8080808c", height: "70px", width: "70px", borderRadius: "5px"}}>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center"}}>6</div>
+                <div className="item-point">
+                    <div >6</div>
                     <div>POINTS</div>
                 </div>
             </Col>
-            <Col span={19}>React Tutorial</Col>
+            <Col span={19}>
+                <div className="item-detail">
+                    <h3 className="detail-text">Hacker News</h3>
+                    <h5>https://hackernews.com</h5>
+                    <div className="vote-buttons">
+                        <Button size="small" icon={<ArrowUpOutlined />} type="text">Up Vote</Button>
+                        <Button size="small" icon={<ArrowDownOutlined />} type="text">Down Vote</Button>
+                    </div>
+                </div>
+            </Col>
         </Row>
         </>
     )
