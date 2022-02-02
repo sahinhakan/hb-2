d68df9e1e4bda4e76651832e7d255e0fa2279452
Hakan Sahin <hakansahince@gmail.com>
2022-02-02 16:42:23 +0300

    adding NewLink component

--- src/components/NewLink.js
+++ src/components/NewLink.js
@@ -1,6 +1,30 @@
+import { Row, Col, PageHeader, Form, Input, Button } from 'antd';
+import { useNavigate } from "react-router-dom";
+import './NewLink.css';
+
 const NewLink = () => {
+    const navigate = useNavigate();
+
     return (
-        <h2>New Link</h2>
+        <div>
+            <Row>
+                <Col span={8} offset={8}>
+                    <PageHeader title="Return to List" className='headerPage' onBack={ () => navigate(-1) }></PageHeader>
+                    <h2>Add New Link</h2>
+                    <Form layout='vertical' size='large'>
+                        <Form.Item label="Link Name" name="linkName">
+                            <Input placeholder='e.g. Alphabet'/>     
+                        </Form.Item>
+                        <Form.Item label="Link URL" name="linkUrl">
+                            <Input placeholder="e.g. http://abc.xyz"/>     
+                        </Form.Item>
+                        <Form.Item wrapperCol={{ span: 6, offset: 18 }}>
+                            <Button type='primary' block="true" shape='round'>ADD</Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
